1489d9a464ae67d06e49c0727e89e60871d1e7e3
Blake245 <jwillden@student.neumont.edu>
2025-02-25 21:37:19 -0700

    Homepage outline almost done

--- vb-frontend/src/app/page.tsx
+++ vb-frontend/src/app/page.tsx
@@ -2,8 +2,19 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div className="video-grid">
+        <div className="video">Video1</div>
+        <div className="video">Video2</div>
+        <div className="video">Video3</div>
+        <div className="video">Video4</div>
+        <div className="video">Video5</div>
+        <div className="video">Video6</div>
+        <div className="video">Video1</div>
+        <div className="video">Video2</div>
+        <div className="video">Video3</div>
+        <div className="video">Video4</div>
+        <div className="video">Video5</div>
+        <div className="video">Video6</div>
+    </div>
   );
 }
