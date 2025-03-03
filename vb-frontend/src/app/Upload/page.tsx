0ce24c6a09c6c40935aa144b92d9c9034240c6a2
Blake245 <jwillden@student.neumont.edu>
2025-03-02 21:57:41 -0700

    Upload Page

--- vb-frontend/src/app/Upload/page.tsx
+++ vb-frontend/src/app/Upload/page.tsx
@@ -1,9 +1,43 @@
-import styles from "./page.module.css";
+"use client";
+import { useRef, useState } from "react";
 
-export default function profile() {
+export default function UploadSection() {
+  const [fileSelected, setFileSelected] = useState(false);
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFileSelected(true);
+    }
+  };
+
   return (
-    <div>
-       <h2>Upload Page</h2> 
-    </div>
+    <main className="center">
+      <div>
+        <h1>Upload Video</h1>
+      </div>
+      <div className="uploadBox" onClick={handleClick}>
+        {!fileSelected && (
+          <>
+            <img src="upload.png" width={200} height={200} alt="Upload Icon" />
+            <h2>Click to upload files</h2>
+          </>
+        )}
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className={fileSelected ? "visible" : "hidden"}
+        />
+      </div>
+      <div>
+        <button className="upload-button" type="submit">Upload</button>
+      </div>
+    </main>
   );
 }
